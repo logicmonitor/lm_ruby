ce5cb2550609e8d28f7c9937d87362aa6cb8786a
Ethan <ecullermayeno@gmail.com>
2013-07-01 15:03:58 -0700

    initial addition. adding some folder structure and some starting ruby files."

---
+++ linux/collector/add_collector.rb
@@ -0,0 +1,20 @@
+# add_collector.rb
+#
+# This is a ruby script to handle the creation and installation of LogicMonitor collectors.
+#
+# Requires:
+# Ruby
+# Ruby gems
+#   json
+# open-url
+# net/http(s)
+#
+# Created for use as a RightScale RightScript.
+# Authorized Sources:
+# LogicMonitor: https://github.com/logicmonitor
+# RightScale Marketplace
+#
+# Author: Ethan Culler-Mayeno
+
+
+
---
+++ linux/collector/remove_collector.rb
@@ -0,0 +1,20 @@
+# remove_collector.rb
+#
+# This is a ruby script to manage the removal of LogicMonitor collectors.
+#
+# Requires:
+# Ruby
+# Ruby gems
+#   json
+# open-url
+# net/http(s)
+#
+# Created for use as a RightScale RightScript.
+# Authorized Sources:
+# LogicMonitor: https://github.com/logicmonitor
+# RightScale Marketplace
+#
+# Author: Ethan Culler-Mayeno
+
+
+
