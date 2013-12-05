6d74d9e6ddf278ae9fe862d0ebc2d4805f60bf5e
Brandon Metcalf <brandon.metcalf@logicmonitor.com>
2013-12-05 12:38:35 -0600

    Made it so we iterate over the returned hostgroups only once.  Before it was doing it 2n where n is the number of lines in the csv.

--- linux/mass-upload/lmMassUpload.rb
+++ linux/mass-upload/lmMassUpload.rb
@@ -10,57 +10,61 @@ date = `date +"%Y%m%d%H%M%S"`
 groupname = "lmsupport-import"
 action=("/rpc/addHostGroup?alertEnable=false&name=#{groupname}")
 
-def newhost
-id=[]
-names=[]
-groupname = "lmsupport-import"
-file= ARGV[0]
- 
- string = apiGet("/rpc/getHostGroups?") #makes API call to grab host group
- hostgroups= JSON.parse(string)
- my_arr=hostgroups['data']
+if (ARGV[0]==nil||ARGV[1]==nil||ARGV[2]==nil||ARGV[3]==nil)
+  puts "\n\nUsage example: 'ruby massupload.rb csvfullPath companyname username password'\n\n\n"
+end
 
-CSV.foreach(file) do |row|
-my_arr.each do |value|
-           if value["fullPath"].eql?(groupname) #matches full path to the csv file
-               id = id.push(value["id"])
-end
-end
-if (row[1]!=nil) #should not be nil anyway for any host to be added, for better logging outputs
- if (row[3]!=nil) #if there is a hostgroup listed
-          my_arr.each do |value|
-           if value["fullPath"].eql?(row[3]) #matches full path to the csv file
-                   if (row[2]!=nil) #if the displayname is nil
-                     action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{value["id"]},#{id[0]}" 
-                     puts "\n Host: " + row[1] 
-                     puts apiGet(action)
-                   else
-                     action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{value["id"]},#{id[0]}"
-                     puts "\n Host: " + row[1]
-	  	     puts apiGet(action)
-                   end
-            end
+def main
+  id    = []
+  names = []
+  file  = ARGV[0]
+ 
+  string = apiGet("/rpc/getHostGroups?") #makes API call to grab host group
+  hostgroups= JSON.parse(string)
+  my_arr=hostgroups['data']
+
+  group = Hash.new
+  my_arr.each do |value|
+    group[value["fullPath"]] = value["id"]
+  end
+
+  CSV.foreach(file) do |row|
+
+    lmgroupid = group["groupname"]
+    if (row[1]!=nil) #should not be nil anyway for any host to be added, for better logging outputs
+      if (row[3]!=nil) #if there is a hostgroup listed
+        if (defined?(group[row[3]].nil?))
+          groupid=group[row[3]]
+          if (row[2]!=nil) #if the displayname is not nil
+            action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{groupid},#{lmgroupid}" 
+            puts "\n Host: " + row[1] 
+            puts apiGet(action)
+          else
+            action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{groupid},#{lmgroupid}"
+            puts "\n Host: " + row[1]
+            puts apiGet(action)
           end
- else
+        end
+      else
         if(row[2]!=nil) #if displayname is nil and there is no fullpath, just place it in root (and the lmsupport-import host group)
-          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{id[0]}"
+          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{lmgroupid}"
           puts "\n Host: " + row[1]
           puts apiGet(action)
         else
-          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{id[0]}"
+          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{lmgroupid}"
           puts "\n Host: " + row[1]
           puts apiGet(action)
-         end
-      
+        end
+      end
+    end
   end
 end
-end
-end
+
 
 def apiGet(action)
-company= ARGV[1]
-username= ARGV[2]
-password= ARGV[3]
+  company= ARGV[1]
+  username= ARGV[2]
+  password= ARGV[3]
 
   url = "https://#{company}.logicmonitor.com/santaba#{action}&c=#{company}&u=#{username}&p=#{password}"
   uri = URI(url)
@@ -75,7 +79,7 @@ end
 apiGet(action)
 
 if (ARGV[0]==nil||ARGV[1]==nil||ARGV[2]==nil||ARGV[3]==nil)
-puts "\n\nUsage example: 'ruby massupload.rb csvfullPath companyname username password'\n\n\n"
+  puts "\n\nUsage example: 'ruby massupload.rb csvfullPath companyname username password'\n\n\n"
 end
-newhost()
 
+main()
