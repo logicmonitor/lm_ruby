9a176ff3d2e8fd2d2f19e888fc8e5c73d18cca36
Brandon Metcalf <brandon.metcalf@logicmonitor.com>
2013-12-05 13:27:13 -0600

    Use timestamped temporary group.

--- linux/mass-upload/lmMassUpload.rb
+++ linux/mass-upload/lmMassUpload.rb
@@ -5,10 +5,6 @@ require 'rbconfig'
 require 'rubygems'
 require 'json'
 
-date = `date +"%Y%m%d%H%M%S"`
-#groupname = "lmsupport-import-"+"#{date}"
-groupname = "lmsupport-import"
-action=("/rpc/addHostGroup?alertEnable=false&name=#{groupname}")
 
 if (ARGV[0]==nil||ARGV[1]==nil||ARGV[2]==nil||ARGV[3]==nil)
   puts "\n\nUsage example: 'ruby massupload.rb csvfullPath companyname username password'\n\n\n"
@@ -18,6 +14,10 @@ def main
   id    = []
   names = []
   file  = ARGV[0]
+
+  date = `date +"%Y%m%d%H%M%S"`
+  (groupname = "lmsupport-import-"+"#{date}").chomp!
+  apiGet("/rpc/addHostGroup?alertEnable=false&name=#{groupname}")
  
   string = apiGet("/rpc/getHostGroups?") #makes API call to grab host group
   hostgroups= JSON.parse(string)
@@ -29,42 +29,38 @@ def main
   end
 
   CSV.foreach(file) do |row|
-
     lmgroupid = group["groupname"]
     if (row[1]!=nil) #should not be nil anyway for any host to be added, for better logging outputs
-      if (row[3]!=nil) #if there is a hostgroup listed
-        if (defined?(group[row[3]].nil?))
-          groupid=group[row[3]]
-          if (row[2]!=nil) #if the displayname is not nil
-            action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{groupid},#{lmgroupid}" 
-            puts "\n Host: " + row[1] 
-            puts apiGet(action)
-          else
-            action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{groupid},#{lmgroupid}"
-            puts "\n Host: " + row[1]
-            puts apiGet(action)
-          end
-        end
-      else
-        if(row[2]!=nil) #if displayname is nil and there is no fullpath, just place it in root (and the lmsupport-import host group)
-          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{lmgroupid}"
-          puts "\n Host: " + row[1]
+      if (defined?(group[row[3]].nil?))
+        groupid=group[row[3]]
+        if (row[2]!=nil) #if the displayname is not nil
+          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{groupid},#{lmgroupid}" 
+          puts "\n Host: " + row[1] 
           puts apiGet(action)
         else
-          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{lmgroupid}"
+          action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{groupid},#{lmgroupid}"
           puts "\n Host: " + row[1]
           puts apiGet(action)
         end
+      end
+    else
+      if(row[2]!=nil) #if displayname is nil and there is no fullpath, just place it in root (and the lmsupport-import host group)
+        action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[2]}&agentId=#{row[0]}&hostGroupIds=#{lmgroupid}"
+        puts "\n Host: " + row[1]
+        puts apiGet(action)
+      else
+        action = "/rpc/addHost?hostName=#{row[1]}&displayedAs=#{row[1]}&agentId=#{row[0]}&hostGroupIds=#{lmgroupid}"
+        puts "\n Host: " + row[1]
+        puts apiGet(action)
       end
     end
   end
 end
 
-
 def apiGet(action)
-  company= ARGV[1]
-  username= ARGV[2]
-  password= ARGV[3]
+  company  = ARGV[1]
+  username = ARGV[2]
+  password = ARGV[3]
 
   url = "https://#{company}.logicmonitor.com/santaba#{action}&c=#{company}&u=#{username}&p=#{password}"
   uri = URI(url)
@@ -75,7 +71,5 @@ def apiGet(action)
   response = http.request(req)
   return response.body
 end
-
-apiGet(action)
 
 main()
